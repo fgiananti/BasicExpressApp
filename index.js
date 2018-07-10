eb5b2493869bc2e447ee3bbc3c1260853dd634c2
francesco <fgiananti@gmail.com>
2018-07-11 00:06:56 +0200

    add create-react-app production rdy

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const indexRoutes = require('./routes/indexRoutes');
+// const indexRoutes = require('./routes/indexRoutes');
 
-app.use('/', indexRoutes);
+// app.use('/', indexRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log('connected')
 })
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-  res.send('Hello World #2');
-});
-
-module.exports = router;
+// const router = require('express').Router();
+//
+// router.get('/', (req, res) => {
+//   res.send('Hello World #2');
+// });
+//
+// module.exports = router;
